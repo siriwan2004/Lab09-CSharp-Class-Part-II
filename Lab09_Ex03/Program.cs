4de08ed6282267a61771db271bb2072b376dd005
siriwan2004 <puttharasusiriwan@gmail.com>
2025-02-14 00:18:28 +0700

    Submit

---
+++ Lab09_Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿Person.name = "Ang";
+    System.Console.WriteLine(Person.GetName());
+    var student = new Student();
+    Student.name = "Siriwan";
+    Console.WriteLine(Student.GetName());
+
+    class Person
+    {
+        public static   string? name;
+        public static string GetName()
+        {
+            return $"Hello from {name}";
+        }
+    }
+
+    class Student : Person
+    {
+        
+    }
---
+++ Lab09_Ex07/Program.cs
@@ -0,0 +1,17 @@
+﻿   double r = 100.0;
+   Circle.PrintCircleArea(r);
+   Circle.PrintCircleCircumference(r);
+   
+   class Circle
+   {
+       const double PI = 3.14159;
+       const double PI2 = PI * 2.0;
+       public static void PrintCircleArea(double radius)
+       {
+           Console.WriteLine($"Radius = {radius}, Area = {PI * radius * radius}");
+       }
+       public static void PrintCircleCircumference(double radius)
+       {
+           Console.WriteLine($"Radius = {radius}, Circumference = {PI2 * radius}");
+       }
+   }
